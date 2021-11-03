fdc995b0994d7b4bcd7c2b08d84d94a3cbd9faf6
Marcelo Cavalcante <mmcant@gmail.com>
2021-11-02 21:14:47 -0300

    rdw dia 01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/global.scss';
+import Header from './components/Header';
+
+
+// jsx
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className="main__wrapper">
+    <div className="main__container">
+      <Header/>
+
+    </div>
+
+  </div>
+  ,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
