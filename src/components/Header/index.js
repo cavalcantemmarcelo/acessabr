b6d5875c5f3eff3bf669de15629cebf1f28b220d
Marcelo Cavalcante <mmcant@gmail.com>
2021-11-04 23:18:06 -0300

    rdw dia 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import './style.scss';
 import logoAcessaBr from '../../images/logo-acessaBR.svg'
 import logoEbac from '../../images/logo-ebac.svg'
 
-const Header = () => {
+const Header = props => {
     return(
         <header className="header__container">
             <div className="header__logo header__logo--state">
                 <img src={logoAcessaBr} alt="Logo AcessaBR"/>
                 <span className="header__city">
-                     / São Paulo - SP
+                     / {props.city} - {props.state}
                 </span>
             </div>
             <div className="header__logo header__logo--ebac">
--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.scss';
 import Header from './components/Header';
+import Home from './pages/Home';
+import Curator from './components/Curator';
 
-
-// jsx
+const CONFIG = {
+  state: 'BA',
+  city: 'Salvador'
+}
 
 ReactDOM.render(
   <div className="main__wrapper">
     <div className="main__container">
-      <Header/>
+      <Header city={CONFIG.city} state={CONFIG.state} />
+      <Home city={CONFIG.city} />
+      {/* Espaço para inserir a lista de lugare */}
+      <Curator />
 
     </div>
 
