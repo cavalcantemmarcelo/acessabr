94b14b3a4471a03b0ca670a26afff761df5fab52
Marcelo Cavalcante <mmcant@gmail.com>
2021-11-18 07:31:56 -0300

    layout responsivo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -21,6 +21,9 @@ const Home = props => {
         <main id="main-content" className="home__container">
             <div className="home__col">
                 <h1 className="home__title">{props.city} para todos</h1>
+                <div className="home__image home__image--destack hide-desktop">
+                    <img src={imageDestack} alt="Imagem destaque cadeirante" />
+                </div>
                 <p className="home__text">Nossa missão é facilitar o encontro entre lugares inclusivos e pessoas que buscam alternativas culturais para sair de casa, com a mobilidade que todo cidadão precisa. </p>
                 <p className="home__text">Checamos 10 itens essenciais de acessibilidade para pessoas com deficiência em diversos pontos da cidade. </p>
                 <div className="home__pills">
@@ -39,7 +42,7 @@ const Home = props => {
                 </div>
             </div>
             <div className="home__col">
-                <div className="home__image home__image--destack">
+                <div className="home__image home__image--destack hide-mobile">
                     <img src={imageDestack} alt="Imagem destaque cadeirante" />
                 </div>
             </div>
