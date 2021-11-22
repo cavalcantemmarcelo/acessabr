924135ffaabd05059024e33020809b3a67306ea0
Marcelo Cavalcante <mmcant@gmail.com>
2021-11-22 00:21:00 -0300

    adiciona api

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,39 +1,54 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import { FilterContext } from '../../contexts/FilterContext'
 import SwiperCore, {Pagination} from 'swiper'
 import { Swiper, SwiperSlide} from 'swiper/react'
 import Card from '../Card'
+import api from '../../config/api'
+
 
 import 'swiper/swiper-bundle.css'
 
 SwiperCore.use([Pagination])
 
 function Slider() {
+    const { filteredPlaces, setFilteredPlaces } = useContext(FilterContext)
+    const [places, setPlaces] = useState([])
+
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            const result = await api.get(`/places?category=${filteredPlaces}`)
+
+            console.log(result)
+
+            if (result.status === 200) {
+                setPlaces(result.data)
+            }
+        }
+
+        fetchPlaces()
+    }, [filteredPlaces])
+
     return(
-        <Swiper slidesPerView={1}
+        <Swiper
             breakpoints={{
+                300: {
+                    slidesPerView: 1
+                },
                 767: {
-                    slidesPerView: 2
+                    slidesPerView: 2 
                 },
                 1024: {
-                    slidesPerView: 4
+                    slidesPerView: 4 
                 }
             }}
         >
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
+            {
+                places.map(item => (
+                    <SwiperSlide key={item.id} >
+                        <Card key={item.id} item={item} />
+                    </SwiperSlide>
+                 ))
+            }
         </Swiper>
     )
 }
